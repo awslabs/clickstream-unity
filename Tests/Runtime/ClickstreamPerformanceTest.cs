e84416d49a644da0fee00d219133bfe9f9779b48
xiaoweii <xiaoweii@amazon.com>
2024-07-11 09:04:37 +0800

    fix: reset ClickstreamAnalytics instance

--- Tests/Runtime/ClickstreamPerformanceTest.cs
+++ Tests/Runtime/ClickstreamPerformanceTest.cs
@@ -1,12 +1,22 @@
 using System.Diagnostics;
+using System.Reflection;
 using NUnit.Framework;
-using UnityEngine;
 
 // ReSharper disable once CheckNamespace
 namespace ClickstreamAnalytics.Tests
 {
     public class ClickstreamPerformanceTests
     {
+        [SetUp]
+        public void SetUp()
+        {
+            var type = typeof(ClickstreamAnalytics);
+            var field = type.GetField("_provider", BindingFlags.NonPublic | BindingFlags.Static);
+            if (field != null)
+            {
+                field.SetValue(null, null);
+            }
+        }
 
         [Test]
         public void TestPerformanceWhenUseMemoryCache()
@@ -20,17 +30,17 @@ namespace ClickstreamAnalytics.Tests
             });
             var stopwatch = new Stopwatch();
             stopwatch.Start();
-            for (var i = 0; i < 10; i++)
+            for (var i = 0; i < 100; i++)
             {
                 ClickstreamAnalytics.Record("testEvent", ClickstreamTests.GetProperties());
             }
 
             stopwatch.Stop();
             var elapsedMilliseconds = (stopwatch.ElapsedTicks / (double)Stopwatch.Frequency) * 1000;
-            UnityEngine.Debug.Log("Save 10 events cost: " + elapsedMilliseconds + " ms");
+            UnityEngine.Debug.Log("Save 100 events cost: " + elapsedMilliseconds + " ms");
             Assert.True(elapsedMilliseconds < 16);
         }
-        
+
         [Test]
         public void TestPerformanceWhenUsePlayerPrefs()
         {
